13347d647422f4bb76270faa799c00c30949365a
ivan-v <navioriv@gmail.com>
2023-05-21 17:46:11 -0400

    Cleanup main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,25 @@
-mod chord;
+use song::generate_song;
 
+mod applied_key;
+mod chord;
+mod chord_progression;
+mod forms;
+mod melody;
+mod motif;
+mod presets;
+mod rhythm;
+mod song;
+
+pub fn sanitize_for_javascript(notes: Vec<(i32, f32, f32)>) -> String {
+    let converted: Vec<String> = notes
+        .iter()
+        .map(|(num, float1, float2)| format!("({}, {}, {})", num, float1, float2))
+        .collect();
+    format!("[{}]", converted.join(", "))
+}
 
 fn main() {
-    println!("Hello, world!");
-    let chord = chord::Chord::new("C".to_owned(), Some("M".to_owned()), None, None);
-    // println!("{} {}", chord.root, chord.pitches);
-    println!("Pitches: {:?}", chord.pitches);
+    let solution = generate_song();
+    println!("{}", sanitize_for_javascript(solution));
 }
 
